c8084f4beb5d04453b5f8ee19b4ded3f07b61fcd
Hector <hectordavidrezende@gmail.com>
2022-09-28 15:02:46 -0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import win32com.client as win
 import pandas as pd
+import graphics as graph
 from funcoes import func
 from variables import var
 ##################################################
@@ -11,7 +12,20 @@ from variables import var
 #  - encerrada
 #  - inssucesso 
 #
-#             -tipo de email-    -para quem sera enviado-       -GMUD-      -Nome da maquina e aplicação-              -Descrição de ações realizadas e motivos-                
-func.cria_email('validacao' ,'hectordavidrezende@gmail.com', 'CHGTANANAN', 'GNCANNL5605 - linux/database', ' A aplicação de patches de S.O. foi realizada com sucesso. Reboot realizado com sucesso e nenhum patch pendente.')
+
+tipo = input('estado de encerramento:\n Opções: validacao, cancelada, inssucesso e encerrada \n')
+para = input('email de validação:\n EX: *****@email.com; *****@outroemail.com.br\n')
+gmud = input('Numero da GMUD\n EX: CHG**** \n')
+host_application = input('Nome do host e a aplicação rodando:\n EX: GNCANNL5605 - linux/database\n')
+desc = input('Escreva a descrição do encerramento/motivos:\n')
+
+
+
+
+
+
+
+#-tipo de email-|-para quem sera enviado-|-GMUD-|-Nome da maquina e aplicação-|-Descrição de ações realizadas e motivos-                
+func.cria_email(tipo,para,gmud,host_application,desc)
 
 print('tarefa comcluida!')
--- variables.py
+++ variables.py
@@ -13,6 +13,7 @@ class var:
     cc_enc_valid = " execucaodemudancas@getnet.com.br; planejamentodemudancas@getnet.com.br; gestao_mudanca@getnet.com.br; suporte@oraex.com; infra_requisicoes_incidentes@getnet.com.br;"
     cc_cancel_ins = "governanca_ti@getnet.com.br; execucaodemudancas@getnet.com.br; planejamentodemudancas@getnet.com.br; soc@getnet.com.br; gestao_mudanca@getnet.com.br; infra_requisicoes_incidentes@getnet.com.br; integracao_demandas@getnet.com.br; suporte@oraex.com"
     cc_teste = "hectordavidrezende@gmail.com; hectordavidrezende@outlook.com.br"
+    lista_teste = ['validacao' ,'hectordavidrezende@gmail.com', 'CHGTANANAN', 'GNCANNL5605 - linux/database', ' A aplicação de patches de S.O. foi realizada com sucesso. Reboot realizado com sucesso e nenhum patch pendente.']
 
 
 
