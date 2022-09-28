2faec571f8a67e3bc221481c337dc2dc6fda6a60
Hector <hectordavidrezende@gmail.com>
2022-09-28 10:38:22 -0300

    <commit_mesage>

--- funcoes.py
+++ funcoes.py
@@ -9,57 +9,108 @@ class func:
             To_emails.append(email)
         return To_emails
     '''
-    def cria_email(tipo ,para ,copia , GMUD ,HOST_service ,descricao ):
+    def cria_email(tipo ,para , GMUD ,HOST_service ,descricao ):
         outlook = win.Dispatch('outlook.application')
         email = outlook.CreateItem(0)
         if tipo == 'validacao':
             email.To = para
-            email.CC = copia
+            email.CC = var.cc_teste
             email.Subject = f'[VALIDAÇÃO] - GMUD: {GMUD} - Adequações de Segurança - {HOST_service}'
             email.HTMLBody = f"""
             <p>Prezados,</p>
             <p class="elementToProof"><br></p>
+            <p class="elementToProof"><br></p>
             <p>Implantada GMUD:</p>
             <p class="elementToProof"><br></p>
-            <p>{GMUD} - Adequações de Segurança - {HOST_service} </p>
+            <p class="elementToProof"><br></p>
+            <p>
+            <b>{GMUD}</b>
+             - Adequações de Segurança - 
+            <b>{HOST_service}</b> 
+            </p>
+            <p class="elementToProof"><br></p>
             <p class="elementToProof"><br></p>
             <p>{descricao}</p>
             <p class="elementToProof"><br></p>
+            <p class="elementToProof"><br></p>
             <p>Favor validar o funcionamento do ambiente.</p>
+            <p class="elementToProof"><br></p>
             <p class="elementToProof"><br></p>
             {var.rodape}
             """
         elif tipo == 'encerrada':
             email.To = para
-            email.CC = copia
+            email.CC = var.cc_teste
             email.Subject = f'[ENCERRADA] - GMUD: {GMUD} - Adequações de Segurança - {HOST_service}'
             email.HTMLBody = f"""
             <p>Prezados,</p>
             <p class="elementToProof"><br></p>
+            <p class="elementToProof"><br></p>
             <p>Implantada GMUD:</p>
             <p class="elementToProof"><br></p>
-            <p>{GMUD} - Adequações de Segurança - {HOST_service} </p>
+            <p class="elementToProof"><br></p>
+            <p>
+            <b>{GMUD}</b>
+             - Adequações de Segurança - 
+            <b>{HOST_service}</b> 
+            </p>
+            <p class="elementToProof"><br></p>
             <p class="elementToProof"><br></p>
             <p>{descricao}</p>
+            <p class="elementToProof"><br></p>
             <p class="elementToProof"><br></p>
             {var.rodape}
             """
         elif tipo == 'cancelada':
             email.To = para
-            email.CC = copia
+            email.CC = var.cc_teste
             email.Subject = f'[CANCELAMENTO] - GMUD: {GMUD} - Adequações de Segurança - {HOST_service}'
             email.HTMLBody = f"""
             <p>Prezados,</p>
             <p class="elementToProof"><br></p>
+            <p class="elementToProof"><br></p>
             <p>GMUD:</p>
             <p class="elementToProof"><br></p>
-            <p>{GMUD} - Adequações de Segurança - {HOST_service} </p>
+            <p class="elementToProof"><br></p>
+            <p>
+            <b>{GMUD}</b>
+             - Adequações de Segurança - 
+            <b>{HOST_service}</b> 
+            </p>
+            <p class="elementToProof"><br></p>
             <p class="elementToProof"><br></p>
             <p>CANCELADA,</p>
             <p class="elementToProof"><br></p>
+            <p class="elementToProof"><br></p>
             <p>{descricao}</p>
+            <p class="elementToProof"><br></p>
             <p class="elementToProof"><br></p>
             {var.rodape}
             """
+        elif tipo == 'inssucesso':
+            email.To = para
+            email.CC = var.cc_teste
+            email.Subject = f'[INSSUCESSO] - GMUD: {GMUD} - Adequações de Segurança - {HOST_service}'
+            email.HTMLBody = f"""
+            <p>Prezados,</p>
+            <p class="elementToProof"><br></p>
+            <p class="elementToProof"><br></p>
+            <p>INSUCESSO da GMUD:</p>
+            <p class="elementToProof"><br></p>
+            <p class="elementToProof"><br></p>
+            <p>
+            <b>{GMUD}</b>
+             - Adequações de Segurança - 
+            <b>{HOST_service}</b> 
+            </p>
+            <p class="elementToProof"><br></p>
+            <p class="elementToProof"><br></p>
+            <p>{descricao}</p>
+            <p class="elementToProof"><br></p>
+            <p class="elementToProof"><br></p>
+            {var.rodape}
+            """
+        else:
+            print('tipo invalido, favor ajustar')
         email.Save()
         return print('email criado  e salvo para anexar evidendias')
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from funcoes import func
 #  - validacao
 #  - cancelada
 #  - encerrada
+#  - inssucesso 
 #
-#             -tipo de email-    -para quem sera enviado-       -Copia de email-          -GMUD-      -Nome da maquina e aplicação-              -Descrição de ações realizadas e motivos-                
-func.cria_email('validacao' ,'hectordavidrezende@gmail.com', 'hector.perez@oraex.com', 'CHGTANANAN', 'GNCANNL5605 - linux/database', ' A aplicação de patches de S.O. foi realizada com sucesso. Reboot realizado com sucesso e nenhum patch pendente.')
+#             -tipo de email-    -para quem sera enviado-       -GMUD-      -Nome da maquina e aplicação-              -Descrição de ações realizadas e motivos-                
+func.cria_email('inssucesso' ,'hectordavidrezende@gmail.com', 'CHGTANANAN', 'GNCANNL5605 - linux/database', ' A aplicação de patches de S.O. foi realizada com sucesso. Reboot realizado com sucesso e nenhum patch pendente.')
 print('tarefa comcluida!')
